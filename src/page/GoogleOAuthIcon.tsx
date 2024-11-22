794b992e04b4422f33cd42040d395a13dd75fd96
JungJihun1012 <jihunjeong965@gmail.com>
2024-11-22 13:03:36 +0900

    Start Project

---
+++ src/page/GoogleOAuthIcon.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const GoogleOAuthIcon = () => {
+    return(
+        <Container>
+
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    flex: 1;
+`;
+export default GoogleOAuthIcon;
